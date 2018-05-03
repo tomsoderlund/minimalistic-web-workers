1181400c69930386aebd93bcdb3ca14630119242
Tom Söderlund <tom@tomsoderlund.com>
2018-05-03 14:58:37 +0200

    Minor code cleanup

--- worker.js
+++ worker.js
@@ -2,13 +2,12 @@ var i = 0;
 
 var timedCount = function () {
 	i = i + 1;
-	postMessage(i);
+	self.postMessage(i);
 	setTimeout(timedCount, 500);
 }
 
 self.addEventListener('message', function (event) {
 	console.log('Inside worker:', event);
-	//self.postMessage(event.data);
 	if (event.data.increment) i += event.data.increment;
 }, false);
 
